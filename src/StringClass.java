4b9184e3d91334388348a26e623704fdd1f49f4a
papanpapan <samim.skmolla@gmail.com>
2017-03-17 16:57:24 +0530

    StringClass
    StringClass

---
+++ src/StringClass.java
@@ -0,0 +1,10 @@
+
+public class StringClass {
+public static void main(String args[]){
+	int IntegerVariable=1000;
+	String StringVariable;
+	StringVariable=Integer.toString(IntegerVariable);
+	System.out.println("The value of IntegerVariable is: "+IntegerVariable);
+	System.out.println("The value of StringVariable is: "+StringVariable);
+  }
+}
